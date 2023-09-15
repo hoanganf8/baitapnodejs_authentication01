c47ed5f548f67a7a3f54ebf94643d0b6b7557743
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-09-15 16:34:53 +0700

    Update code

---
+++ controllers/AuthController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const md5 = require("md5");
+
+module.exports = {
+  index: async (req, res) => {
+    const msg = req.flash("msg");
+
+    res.render("auth/login", { msg });
+  },
+
+  handeLogin: async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email, password: md5(password) },
+    });
+
+    if (user?.dataValues) {
+      //login success
+      req.session.userLogin = user.dataValues;
+    } else {
+      //login failed
+      req.flash("msg", "Email hoặc mật khẩu không chính xác");
+    }
+
+    res.redirect("/auth/login");
+  },
+
+  logout: (req, res) => {
+    req.session.destroy();
+    res.redirect("/auth/login");
+  },
+};
